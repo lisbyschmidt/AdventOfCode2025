aa7c29b064c6dc62ed0cf296577db022c0f5b2ff
Lisby Schmidt Soren <soren.lisbyschmidt@hm.com>
2025-12-02 09:59:44 +0100

    day01

---
+++ day01/README.md
@@ -0,0 +1,8 @@
+# Day 1: Secret Entrance
+https://adventofcode.com/2025/day/1
+
+## How to run locally
+```bash
+cd day01/
+dotnet run solution.cs ./input.txt
+```
---
+++ day01/solution.cs
@@ -0,0 +1,34 @@
+﻿var numberSequences = File
+    .ReadLines(args[0])
+    .Select(inputLine => new Rotation(inputLine[0], int.Parse(inputLine[1..])))
+    .Aggregate<Rotation, IEnumerable<int>[]>(
+        seed: [[50]],
+        func: (numberSequences, rotation) =>
+            [..numberSequences, rotation.Numbers(from: numberSequences.Last().Last())]
+    );
+
+Console.WriteLine($"Part 1: {numberSequences.Count(x => x.Last() is 0)}");
+Console.WriteLine($"Part 2: {numberSequences.SelectMany(x => x).Count(x => x is 0)}");
+
+record Rotation(char Direction, int Distance)
+{
+    public IEnumerable<int> Numbers(int from)
+    {
+        var current = from;
+        for (var i = 0; i < Distance; i++)
+        {
+            current = Direction switch
+                {
+                    'L' => current - 1,
+                    'R' => current + 1,
+                    _ => throw new ArgumentOutOfRangeException($"{nameof(Direction)} '{Direction}'")
+                } switch
+                {
+                    < 0 => 99,
+                    > 99 => 0,
+                    var x => x
+                };
+            yield return current;
+        }
+    }
+}
