4d67879046a2db2d677db2d8702a99df0d98a224
Lisby Schmidt Soren <soren.lisbyschmidt@hm.com>
2025-12-04 13:09:45 +0100

    day03

---
+++ day03/README.md
@@ -0,0 +1,8 @@
+# Day 3: Lobby
+https://adventofcode.com/2025/day/3
+
+## How to run locally
+```bash
+cd day03/
+dotnet run solution.cs ./input.txt
+```
---
+++ day03/solution.cs
@@ -0,0 +1,19 @@
+﻿var batteryBanks = File.ReadLines(args[0]).ToArray();
+Console.WriteLine($"Part 1: {batteryBanks.Select(x => LargestJoltage(x, 2)).Sum()}");
+Console.WriteLine($"Part 2: {batteryBanks.Select(x => LargestJoltage(x, 12)).Sum()}");
+
+static int IndexOfMax(string digits) =>
+    Enumerable.Range(0, digits.Length).MaxBy(x => digits[x]);
+
+static long LargestJoltage(string digits, int length)
+{
+    var (index, result) = (-1, "");
+    do
+    {
+        var digitsInScope = digits[(index + 1)..^(length - result.Length - 1)];
+        index = IndexOfMax(digitsInScope) + index + 1;
+        result += $"{digits[index]}";
+    } while (result.Length < length);
+
+    return long.Parse(result);
+}
