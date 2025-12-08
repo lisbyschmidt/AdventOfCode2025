fa621f7332a5569bb603061ea3b60e75886ca8d1
Søren Lisby Schmidt <31357411+lisbyschmidt@users.noreply.github.com>
2025-12-08 23:57:46 +0100

    day07

---
+++ day07/README.md
@@ -0,0 +1,8 @@
+# Day 7: Laboratories
+https://adventofcode.com/2025/day/7
+
+## How to run locally
+```bash
+cd day07/
+dotnet run solution.cs ./input.txt
+```
---
+++ day07/solution.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Concurrent;
+using Position = (int row, int col);
+
+var rows = File.ReadLines(args[0]).ToArray();
+
+Console.WriteLine($"Part 1: {Splits(rows, [], (0, rows[0].IndexOf('S')))}");
+Console.WriteLine($"Part 2: {Timelines(rows, [], (0, rows[0].IndexOf('S')))}");
+
+static long Splits(string[] rows, HashSet<(int, int)> cache, Position start) =>
+    NextSplitter(rows, start) is { } splitter && cache.Add(splitter)
+        ? Splits(rows, cache, (splitter.row, splitter.col - 1)) +
+          Splits(rows, cache, (splitter.row, splitter.col + 1)) +
+          1
+        : 0;
+
+static long Timelines(string[] rows, ConcurrentDictionary<(int, int), long> cache, Position start) =>
+    cache.GetOrAdd(start, pos =>
+        NextSplitter(rows, pos) is { } splitter
+            ? Timelines(rows, cache, (splitter.row, splitter.col - 1)) +
+              Timelines(rows, cache, (splitter.row, splitter.col + 1))
+            : 1
+    );
+
+static Position? NextSplitter(string[] rows, Position start) => Enumerable
+        .Range(start.row + 1, rows.Length - start.row - 1)
+        .TakeWhile(row => rows[row][start.col] is '.')
+        .Last() + 1
+    is var endRow && endRow < rows.Length
+    ? (endRow, start.col)
+    : null;
